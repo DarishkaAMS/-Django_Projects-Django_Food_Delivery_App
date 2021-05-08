831565a5ec9f0b6b8af767590fdc7ed7e1aeaece
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-08 17:36:45 +0300

    Update url mapping

--- MainProject/MainApp/urls.py
+++ MainProject/MainApp/urls.py
@@ -3,4 +3,13 @@ from .views import home_page_view
 
 urlpatterns = [
     path('', home_page_view, name='index'),
+    path('', home_page_view, name='about'),
+    path('', home_page_view, name='about'),
+    path('', home_page_view, name='services'),
+    path('', home_page_view, name='portfolio-1'),
+    path('', home_page_view, name='single-project'),
+    path('', home_page_view, name='single-project-2'),
+    path('', home_page_view, name='blog-1'),
+    path('', home_page_view, name='single-post'),
+    path('', home_page_view, name='contact'),
 ]
