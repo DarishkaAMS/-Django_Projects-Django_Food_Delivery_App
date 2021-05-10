946871daca8fdb2e8da13a6c8adc9f251037056b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-10 20:27:21 +0300

    Add AUTHENTICATION_BACKENDS

--- MainProject/MainProject/settings.py
+++ MainProject/MainProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'account.Account'
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.AllowAllUsersModelBackend'
+    'django.contrib.auth.backends.AllowAllUsersModelBackend',
     'account.backends.CaseInsensitiveModelBackend'
 )
 
--- MainProject/account/backends.py
+++ MainProject/account/backends.py
@@ -9,8 +9,8 @@ class CaseInsensitiveModelBackend(ModelBackend):
         if username is None:
             username = kwargs.get(UserModel.USERNAME_FIELD)
         try:
-            case_insensitive_user_field = '{}__iexact'.format(UserModel.USERNAME_FIELD)
-            user = UserModel._default_manager.get(**{case_insensitive_user_field: username})
+            case_insensitive_username_field = '{}__iexact'.format(UserModel.USERNAME_FIELD)
+            user = UserModel._default_manager.get(**{case_insensitive_username_field: username})
         except UserModel.DoesNotExist:
             UserModel().set_password(password)
         else:
