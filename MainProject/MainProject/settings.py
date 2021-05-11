13021d7932d3f94fbfca8e89fb4eb256db6773dc
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-11 11:48:13 +0300

    Add static and media

--- MainProject/MainProject/settings.py
+++ MainProject/MainProject/settings.py
@@ -140,4 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_BIR, 'static_cdn')
+
+# MEDIA_URL = '/media'
+# MEDIA_ROOT = os.path.join(BASE_BIR, 'media_cdn')
+
