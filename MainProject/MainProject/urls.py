7a4d02c34fa147e63599445c9db3a51268df31a9
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-12 06:33:57 +0300

    Update static and media roots + collect static

--- MainProject/MainProject/urls.py
+++ MainProject/MainProject/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MainApp.urls')),
 ]
-#
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
