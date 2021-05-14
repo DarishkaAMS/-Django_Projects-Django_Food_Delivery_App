35ec4b982f5c643ecc5cdfeb527bd4eaa7e5fc4a
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-14 07:37:58 +0300

    Add password reset folder and correct rooting

--- MainProject/account/urls.py
+++ MainProject/account/urls.py
@@ -8,16 +8,17 @@ urlpatterns = [
     path('register/', register_page_view, name='register'),
 
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='password_change_done.htm'), name='password_change_done'),
+        template_name='password_reset/password_change_done.htm'), name='password_change_done'),
     path('password_change/', auth_views.PasswordChangeView.as_view(
-        template_name='password_change.html'), name='password_change'),
+        template_name='password_reset/password_change.html'), name='password_change'),
 
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='password_reset_done.html'), name='password_reset_done'),
+        template_name='password_reset/password_reset_done.html'), name='password_reset_done'),
+
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
     path('password_reset/', auth_views.PasswordResetView.as_view(),
          name='password_reset'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='password_reset_complete.html'), name='password_reset_complete'),
+        template_name='password_reset/password_reset_complete.html'), name='password_reset_complete'),
 ]
