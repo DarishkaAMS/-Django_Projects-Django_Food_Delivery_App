9bcf2420c3c56b36f96cf594b497662130292d2d
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-14 07:19:41 +0300

    Add password/email change/reset urls

--- MainProject/account/urls.py
+++ MainProject/account/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import login_page_view, logout_view, register_page_view
 
@@ -5,4 +6,18 @@ urlpatterns = [
     path('login/', login_page_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_page_view, name='register'),
-    ]
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='password_change_done.htm', name='password_change_done')),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='password_change.html', name='password_change')),
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_done.html', name='password_reset_done')),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html', name='password_reset_complete')),
+]
